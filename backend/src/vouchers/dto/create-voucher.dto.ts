a6987da59bb43fa0f51e5c3d3bd03790622f284e
Benjamin Espinoza <bespinoza2021@alu.uct.cl>
2025-12-18 13:51:15 -0300

    feat: :art: Eliminacion de cosas que no se ocupaban

--- backend/src/vouchers/dto/create-voucher.dto.ts
+++ backend/src/vouchers/dto/create-voucher.dto.ts
@@ -1,8 +1,7 @@
-import { IsInt, IsPositive, Min } from 'class-validator';
+import { IsInt, Min } from 'class-validator';
 
 export class CreateVoucherDto {
   @IsInt()
-  @IsPositive()
   @Min(1)
   kilos: number; // 15 o 45 kg
 }
