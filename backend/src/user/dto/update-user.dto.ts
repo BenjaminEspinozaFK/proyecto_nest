ad6501570972a873d5e83e687f5e279635bcaa57
Benjamin Espinoza <bespinoza2021@alu.uct.cl>
2026-09-07 02:06:16 -0300

    feat: :art: Se corrige IDOR en UsersController que permitía tomar el control de cuentas ajenas

--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -16,12 +16,6 @@ export class UpdateUserDto {
   @Transform(({ value }) => value.trim())
   @IsString()
   @IsOptional()
-  @MinLength(6)
-  password?: string;
-
-  @Transform(({ value }) => value.trim())
-  @IsString()
-  @IsOptional()
   @MinLength(2, { message: 'El nombre debe tener al menos 2 caracteres' })
   name?: string;
 
